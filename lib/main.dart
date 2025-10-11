51d35bb717ae987abb89b41116f4126d248f2ad6
Mattcodez21 <166388565+Mattcodez21@users.noreply.github.com>
2025-10-11 14:35:01 +0100

    Adjusting Size of Container

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,8 @@ class OrderItemDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 200,
-        height: 100,
+        width: 300,
+        height: 200,
         color: Colors.blue,
         child: Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}'));
   }
